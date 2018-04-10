ec7e77cde8e4fc611e518ddf8bb2ea489a9367d7
Xigndong Bian <xingdong.bian@earlham.ac.uk>
2018-04-10 12:06:22 +0100

    now able to display from elastic search

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+
+
+function generate_random_id() {
+    var id = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 10; i++)
+        id += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return id;
+}
+
+function isInArray(value, array) {
+    return array.indexOf(value) > -1;
+}
+
